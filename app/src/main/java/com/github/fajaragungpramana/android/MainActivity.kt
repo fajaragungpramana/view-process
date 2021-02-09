c76b69fa50150e55d7c91fecdd27e68d9dce5fce
fajaragungpramana <fajar.agungpramana01@gmail.com>
2021-02-09 23:27:53 +0700

    Adding some examples.

--- app/src/main/java/com/github/fajaragungpramana/android/MainActivity.kt
+++ app/src/main/java/com/github/fajaragungpramana/android/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.github.fajaragungpramana.android
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.github.fajaragungpramana.viewprocess.ViewProcess
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewProcess = findViewById<ViewProcess>(R.id.view_process)
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val btnStop = findViewById<Button>(R.id.btn_stop)
+
+        btnStart.setOnClickListener {
+            viewProcess.start
+        }
+
+        btnStop.setOnClickListener {
+            viewProcess.stop
+        }
+
     }
 
 }
